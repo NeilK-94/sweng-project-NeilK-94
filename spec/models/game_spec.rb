8c6fc3b23298074200e6b14956dd539b4585a832
NeilK-94 <neil.kyne@gmail.com>
2020-05-13 12:53:54 +0000

    Added title, foundation for rspec tests on controller and model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Game do
+  it "is valid with valid attributes"
+  it "is not valid without a title"
+  it "is not valid without a genre"
+  it "is not valid without a score"
+  it "is not valid without a developer"
 end
