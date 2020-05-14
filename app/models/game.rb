c1279fc9a484022ece29b58987ed3dda94adb0ba
NeilK-94 <neil.kyne@gmail.com>
2020-05-14 11:19:05 +0000

    Some unit testing on game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ActiveRecord::Base
-  
+    validates :title, presence: true
+    validates :score, presence: true
+
   def self.find_with_same_developer(developer)
     Game.where(:developer => developer) 
   end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 describe Game do
-  it "is valid with valid attributes"
-  it "is not valid without a title"
-  it "is not valid without a genre"
-  it "is not valid without a score"
-  it "is not valid without a developer"
+  context 'validation test on game' do
+    it 'ensures title presence' do
+        game = Game.new(genre: 'test', score: 54, developer: 'test').save
+        expect(game).to eq(false)   #   title presence is set to required in 'game.rb'
+    end
+    it 'ensures score presence' do
+        game = Game.new(title: 'test', genre: "test", developer: 'test').save
+        expect(game).to eq(false)   #   score presence is set to required in 'game.rb'
+    end
+  end
 end
