60788b86ec9ae8af015249e600163979a623229e
NeilK-94 <neil.kyne@gmail.com>
2020-05-15 10:54:11 +0000

    More unit tests on model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
-    validates :title, presence: true
-    validates :score, presence: true
+    validates :title, presence: true, length: { in: 3..35 }
+    validates :score, presence: true, :numericality => { :greater_than => 0, :less_than => 100 }
 
   def self.find_with_same_developer(developer)
     Game.where(:developer => developer) 
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,14 +1,36 @@
 require 'rails_helper'
 
 describe Game do
-  context 'validation test on game' do
+    subject {
+        Game.new(
+            title: "test",
+            genre: "test",
+            score: 43,
+            developer: "test",
+        )
+    }
+  context 'validation test on game title' do
     it 'ensures title presence' do
-        game = Game.new(genre: 'test', score: 54, developer: 'test').save
-        expect(game).to eq(false)   #   title presence is set to required in 'game.rb'
+        subject.title = nil
+        expect(subject).to_not be_valid   #   title presence is set to required in 'game.rb'
     end
-    it 'ensures score presence' do
-        game = Game.new(title: 'test', genre: "test", developer: 'test').save
-        expect(game).to eq(false)   #   score presence is set to required in 'game.rb'
+    it 'ensures title minimum length' do
+        subject.title = "a"
+        expect(subject).to_not be_valid   #   title length minimum is 3
+    end
+    it 'ensures title maximum length' do
+        subject.title = "a" * 40
+        expect(subject).to_not be_valid   #   title length maximum is 40
     end
   end
+  context 'validation test on game score' do
+    it 'ensures score presence' do
+        subject.score = nil
+        expect(subject).to_not be_valid   #   score presence is set to required in 'game.rb'
+    end
+    it 'ensures score presence is in valid number range' do
+        subject.score = 101
+        expect(subject).to_not be_valid   #   score must be between 1...100
+    end
+    end
 end
